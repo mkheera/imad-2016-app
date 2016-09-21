9fe06bf210daeb3d0fa833976d7ba0702419da84
mkheera <mkheera@yahoo.com>
2016-09-22 04:15:50 +1000

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,6 +8,15 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+app.get('/article-one', function(req, res){
+    res.send('atricle one requested');
+});
+app.get('/article-two', function(req, res){
+    res.send('atricle two requested');
+});
+app.get('/article-three', function(req, res){
+    res.send('atricle three requested');
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
