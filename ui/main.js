8cb5062bef6f31a98b0c3997d3e21af5da0d611f
mkheera <mkheera@yahoo.com>
2016-10-01 15:12:06 +1000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -16,6 +16,6 @@ var counter=0;
 button.onclick=function(){
     counter=counter+1;
     var span=document.getElementById('count');
-    span.InnerHTML=counter.toString();
+    span.innerHTML=counter.toString();
     
 };
