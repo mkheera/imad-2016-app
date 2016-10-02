b3b35f1a8878cab25adf1ece10640dc00a606357
mkheera <mkheera@yahoo.com>
2016-10-03 03:59:16 +1000

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -104,7 +104,7 @@ var counter = 0;
  });
  
  var names=[];
-app.get('/submit-name', function(req, res){
+app.get('/submit-name/:name', function(req, res){
     var name=req.query.name;
 names.push(name);
 res.send(JSON.stringify('names'));
