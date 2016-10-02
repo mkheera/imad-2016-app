b32afe9af7c4c0113b9bc1dd0788db187177882f
mkheera <mkheera@yahoo.com>
2016-10-03 03:22:05 +1000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -43,7 +43,7 @@ submit.onclick=function(){
     
     var names=['name1', 'name2', 'name3', 'name4'];
     var list='';
-    for(var i=0;i<names.length;i++){
+    for(var i=0;i<=names.length;i++){
         list='<li>'+names[i]+'</li>';
         var ul=document.getElementById('namelist');
         ul.innerHTML=list;
