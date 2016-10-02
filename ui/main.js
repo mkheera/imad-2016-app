4d70b45bfa20f818bbe970ec1cce9e8415f3dbaf
mkheera <mkheera@yahoo.com>
2016-10-03 03:50:08 +1000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -41,11 +41,12 @@ var name=nameInput.value;
 var submit=document.getElementById('submit-btn');
 submit.onclick=function(){
     
-    var names=['name1', 'name2', 'name3', 'name4'];
+    var names=['name1', 'name2', 'name3'];
     var list='';
     for(var i=0;i<names.length;i++){
         list='<li>'+names[i]+'</li>';
+    }
         var ul=document.getElementById('namelist');
         ul.innerHTML=list;
-    }
+    
 }
