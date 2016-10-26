66e50cefacaf1eaf57ec7c79749f7d5b708e5574
mkheera <mkheera@yahoo.com>
2016-10-26 14:57:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -126,7 +126,7 @@ app.get('/ui/main.js', function (req, res) {
 });
 
 app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+  res.sendFile(path.join(__dirname, 'ui', 'heera.jpg'));
  
  
 });
