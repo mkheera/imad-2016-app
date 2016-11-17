e8363ad71016b6469601a84c11ebe13bd1ba7b7d
mkheera <mkheera@yahoo.com>
2016-11-17 23:40:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -135,7 +135,7 @@ res.send(JSON.stringify(names));
 
 app.get('/articles/:articleName', function(req, res){
     
-    pool.query(" SELECT * FROM article WHERE title= '" + req.params.articleName +"'", function(err, result){
+    pool.query(" SELECT * FROM article WHERE title= $1",[req.params.articleName] , function(err, result){
         if(err){
          res.status(500).send(err.toString());
      }
