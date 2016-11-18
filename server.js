d63913bee1cec33288918b2c3682c439ed77fc30
mkheera <mkheera@yahoo.com>
2016-11-18 23:21:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -105,7 +105,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 function hash(input,salt){
-    var hashed=crypto.pbkdf2sync(input,salt,1000,512,'sha512');
+    var hashed=crypto.pbkdf2Sync(input,salt,1000,512,'sha512');
     return hashed.toString('hex');
 }
 
