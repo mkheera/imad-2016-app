3be0cd8786806669b0a1c2730906c0c0cfbff15c
mkheera <mkheera@yahoo.com>
2016-11-22 23:39:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ var path = require('path');
 var Pool= require('pg').Pool;
 var crypto =require('crypto');
 var bodyParser=require('body-parser');
+var session = require('express-session');
+
+
 var config={
     user:'mkheera',
     database:'mkheera',
@@ -14,7 +17,10 @@ var config={
 var app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json());
-
+app.use(session({
+    secret: 'someRandomSecretValue',
+    cookie: { maxAge: 1000 * 60 * 60 * 24 * 30}
+}));
 
 
 function createTemplate(data){
