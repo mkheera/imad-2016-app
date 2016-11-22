229c72b4c63d8315868d4a691ae7898517d08630
mkheera <mkheera@yahoo.com>
2016-11-23 00:19:34 +0530

    [imad-console] Updates ui/article.js

--- ui/article.js
+++ ui/article.js
@@ -1,8 +1,37 @@
-var currentArticleTitle = window.location.pathname.split('/')[2];
-var commentFormHtml = `
-        <h5>Submit a comment</h5>
-        <textarea id="comment_text" rows="5" cols="100" placeholder="Enter your comment here..."></textarea>
-        <br/>
-        <input type="submit" id="submit" value="Submit" />
-        <br/>
-        `;
+var submit=document.getElementById('submit');
+
+submit.onclick=function(){
+    
+                var request=new XMLHttpRequest();
+    
+                 request.onreadystatechange=function(){
+        
+        
+                          if(request.readyState===XMLHttpRequest.DONE){
+            
+        
+                                    if(request.status===200){
+                                            var names=request.responseText;
+                                            names=JSON.parse(names);
+                                            var list='';
+                                            for(var i=0;i<names.length;i++){
+                                                list+='<li>'+names[i]+'</li>';
+                                            }
+                                             var ul=document.getElementById('namelist');
+                                              ul.innerHTML=list;
+    
+                                     }
+                
+                          }
+                 };
+                 var nameInput=document.getElementById('name');
+                 var name=nameInput.value;
+                 request.open('GET','http://mkheera.imad.hasura-app.io/submit-name?name=' +name,true);
+                 request.send(null);
+   
+
+
+
+       
+    
+};
