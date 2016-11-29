ece693f561aff93a6495d65b6f63a90f9e04823d
mkheera <mkheera@yahoo.com>
2016-11-29 23:30:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -70,7 +70,7 @@ submit.onclick=function(){
 var username=document.getElementById('username').value;
 var password=document.getElementById('password').value;
 console.log('username');
-alert('username');
+
 
  request.open('POST','http://mkheera.imad.hasura-app.io/login' ,true);
  request.setRequestHeader('Content-Type','application/json');
