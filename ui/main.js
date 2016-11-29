5782765622d9de6cdb3b91532f27761a3537772f
mkheera <mkheera@yahoo.com>
2016-11-29 23:21:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -64,10 +64,7 @@ submit.onclick=function(){
                 
                           }
                  };
-                 var nameInput=document.getElementById('name');
-                 var name=nameInput.value;
-                 request.open('GET','http://mkheera.imad.hasura-app.io/submit-name?name=' +name,true);
-                 request.send(null);
+                
    
 
 var username=document.getElementById('username').value;
